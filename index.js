3771155922f32d9f46f30ee096ee1cb9a598abb3
Nora Gaughan <noragaughan@gmail.com>
2020-07-13 16:56:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-function updateObjectWithKeyAndValue(object, key, value) {return Object.assign({}, obj, { [key]: value })}
+function updateObjectWithKeyAndValue(object, key, value) {return Object.assign({}, object, { [key]: value })}
