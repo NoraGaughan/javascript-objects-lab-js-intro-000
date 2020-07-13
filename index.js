c67b70622533d9b5eeed2900db5ecbfcbc3457d9
Nora Gaughan <noragaughan@gmail.com>
2020-07-13 17:08:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function updateObjectWithKeyAndValue(object, key, value) {return Object.assign({}, object, { [key]: value })}
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) { object[key] = value;
 return object} 
-function deleteFromObjectByKey(object, key) {
+function deleteFromObjectByKey(object, key) { var = newObj 
